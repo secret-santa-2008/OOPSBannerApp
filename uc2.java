f145a444bfa99fcd17a57363576c052d55808558
secret-santa-2008 <nishitac1018@gmail.com>
2026-02-17 15:10:27 +0530

    Update Java files

--- uc2.java
+++ uc2.java
@@ -1,4 +1,4 @@
-public class BannerApp {
+public class uc2 {
     public static void main(String[] args) {
 
         System.out.println("  ***     ***     *****  ");
--- us1.java
+++ us1.java
@@ -1,4 +1,4 @@
-class oops{
+class uc1{
     public static void main (String[] args) {
         System.out.println("OOPS");
     }
