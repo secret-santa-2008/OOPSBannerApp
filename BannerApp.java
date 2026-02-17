8e25d4fa119cb9a6fc8aa59e94eda88cb9b6db53
secret-santa-2008 <nishitac1018@gmail.com>
2026-02-17 14:46:00 +0530

    Added BannerApp program

---
+++ BannerApp.java
@@ -0,0 +1,11 @@
+public class BannerApp {
+    public static void main(String[] args) {
+
+        System.out.println("  ***     ***     *****  ");
+        System.out.println(" *   *   *   *    *      ");
+        System.out.println(" *   *   *   *    ****   ");
+        System.out.println(" *   *   *   *    *      ");
+        System.out.println("  ***     ***     *****  ");
+
+    }
+}
