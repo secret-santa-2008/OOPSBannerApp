54376aec22e44c0352d73a9aa5f623129cd5b862
secret-santa-2008 <nishitac1018@gmail.com>
2026-02-17 15:18:37 +0530

    UC3 - Refactored banner using String.join()

---
+++ uc3.java
@@ -0,0 +1,39 @@
+public class uc3{
+
+    public static void main(String[] args) {
+
+        // Letter O
+        System.out.println(String.join("", " ***** "));
+        System.out.println(String.join("", "*     *"));
+        System.out.println(String.join("", "*     *"));
+        System.out.println(String.join("", "*     *"));
+        System.out.println(String.join("", " ***** "));
+
+        System.out.println();
+
+        // Letter O
+        System.out.println(String.join("", " ***** "));
+        System.out.println(String.join("", "*     *"));
+        System.out.println(String.join("", "*     *"));
+        System.out.println(String.join("", "*     *"));
+        System.out.println(String.join("", " ***** "));
+
+        System.out.println();
+
+        // Letter P
+        System.out.println(String.join("", "****** "));
+        System.out.println(String.join("", "*     *"));
+        System.out.println(String.join("", "****** "));
+        System.out.println(String.join("", "*      "));
+        System.out.println(String.join("", "*      "));
+
+        System.out.println();
+
+        // Letter S
+        System.out.println(String.join("", " ***** "));
+        System.out.println(String.join("", "*      "));
+        System.out.println(String.join("", " ***** "));
+        System.out.println(String.join("", "      *"));
+        System.out.println(String.join("", " ***** "));
+    }
+}
